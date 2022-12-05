86768aa5d56708c4d8c0b279722e37711dbfbe07
TatianaKokunina <tanechka.91@mail.ru>
2022-12-05 10:53:54 +0300

    EX64

---
+++ EX64/Program.cs
@@ -0,0 +1,26 @@
+﻿/*  Задайте значение N. Напишите программу, которая выведет все натуральные
+ числа в промежутке от N до 1. Выполнить с помощью рекурсии.*/
+/*int getUserInt(string message)
+{
+    Console.ForegroundColor = ConsoleColor.DarkCyan;
+    Console.WriteLine(message);
+    Console.ResetColor();
+    int userInt = Convert.ToInt32(Console.ReadLine());
+    return userInt;
+}
+getUserInt("Введите N");
+int Startnumber = getUserInt;*/
+
+
+Console.Write("Введите N: "); 
+int number = Convert.ToInt32(Console.ReadLine()); 
+
+void NumberCounter(int number)
+{
+    if (number < 0) Console.WriteLine($"{number} не натуральное число");
+    if (number == 0) return;
+    Console.Write("{0,4}", number);
+    NumberCounter(number - 1);
+}
+
+NumberCounter(number);
